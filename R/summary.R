c1989953f6ebca79cbfe61e8518c14279788a503
Adam Smith <avianmigration@gmail.com>
2017-08-02 18:52:26 +0000

    Tidy messaging

--- R/summary.R
+++ R/summary.R
@@ -1,7 +1,5 @@
 #' fwsinat summary
 #'
-#'
-#'
 #' @param object \code{fwsinat} \code{data.frame} of iNaturalist observations
 #'  produced by running \code{\link{retrieve_inat}}, and (optionally) subsequently through
 #'  \code{\link{assign_inat}} and/or \code{\link{update.fwsinat}}
@@ -10,8 +8,8 @@
 #'  iNaturalist projects and the date of the retrieval.
 #' @export
 summary.fwsinat <- function(object, ...) {
-  message(deparse(substitute(object)), " contains the following iNaturalist project(s):")
-  message(paste0("  ", attr(object, "inat_proj"), "\n"))
+  message(deparse(substitute(object)), " contains the following iNaturalist project(s):",
+          paste0("\n  ", attr(object, "inat_proj")))
   message("iNaturalist data retrieval was initiated at ",
           strftime(attr(object, "query_dt"), format = "%Y-%m-%d %H:%M:%S", usetz = TRUE))
   NextMethod()
